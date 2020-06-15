3e8ce210336c9f8e7cdce43629f6967f5b895235
Farid Safi <FaridSafi@noreply.github.com>
2020-06-15 13:52:42 +0200

    initial

---
+++ App.js
@@ -0,0 +1,130 @@
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './containers/HomeScreen';
+import ProfileScreen from './containers/ProfileScreen';
+import SignInScreen from './containers/SignInScreen';
+import SignUpScreen from './containers/SignUpScreen';
+import SettingsScreen from './containers/SettingsScreen';
+
+Icon.loadFont();
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(null);
+
+  const setToken = async (token) => {
+    if (token) {
+      AsyncStorage.setItem('userToken', token);
+    } else {
+      AsyncStorage.removeItem('userToken');
+    }
+
+    setUserToken(token);
+  };
+
+  useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      // We should also handle error for production apps
+      const userToken = await AsyncStorage.getItem('userToken');
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      setIsLoading(false);
+      setUserToken(userToken);
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      {isLoading ? null : userToken === null ? ( // We haven't finished checking for the token yet
+        // No token found, user isn't signed in
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            options={{headerShown: false, animationEnabled: false}}>
+            {() => <SignInScreen setToken={setToken} />}
+          </Stack.Screen>
+          <Stack.Screen name="SignUp">
+            {() => <SignUpScreen setToken={setToken} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      ) : (
+        // User is signed in
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Tab"
+            options={{headerShown: false, animationEnabled: false}}>
+            {() => (
+              <Tab.Navigator
+                tabBarOptions={{
+                  activeTintColor: 'tomato',
+                  inactiveTintColor: 'gray',
+                }}>
+                <Tab.Screen
+                  name="Home"
+                  options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({color, size}) => {
+                      return (
+                        <Icon name={'ios-home'} size={size} color={color} />
+                      );
+                    },
+                  }}>
+                  {() => (
+                    <Stack.Navigator>
+                      <Stack.Screen
+                        name="Home"
+                        options={{
+                          title: 'My App',
+                          headerStyle: {backgroundColor: 'red'},
+                          headerTitleStyle: {color: 'white'},
+                        }}>
+                        {() => <HomeScreen />}
+                      </Stack.Screen>
+
+                      <Stack.Screen
+                        name="Profile"
+                        options={{
+                          title: 'User Profile',
+                        }}>
+                        {() => <ProfileScreen />}
+                      </Stack.Screen>
+                    </Stack.Navigator>
+                  )}
+                </Tab.Screen>
+                <Tab.Screen
+                  name="Settings"
+                  options={{
+                    tabBarLabel: 'Settings',
+                    tabBarIcon: ({color, size}) => (
+                      <Icon name={'ios-options'} size={size} color={color} />
+                    ),
+                  }}>
+                  {() => (
+                    <Stack.Navigator>
+                      <Stack.Screen
+                        name="Settings"
+                        options={{title: 'Settings', tabBarLabel: 'Settings'}}>
+                        {() => <SettingsScreen setToken={setToken} />}
+                      </Stack.Screen>
+                    </Stack.Navigator>
+                  )}
+                </Tab.Screen>
+              </Tab.Navigator>
+            )}
+          </Stack.Screen>
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
